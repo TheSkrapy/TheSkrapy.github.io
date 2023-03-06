27d9e84b7d83bddf0622122f47aa4104922ad96e
TheSkrapy <gregocastillo07@hotmail.com>
2023-03-06 07:45:02 -0600

    Portafolio base terminado, falta personalizacion

---
+++ script.js
@@ -0,0 +1,44 @@
+let menuVisible = false;
+//Funcion que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList="";
+        menuVisible=false;
+    }
+    else{
+        document.getElementById("nav").classList="responsive";
+        menuVisible=true;
+    }
+}
+
+function seleccinar(){
+    //Ocualta el menu una vez selecciono una opcion 
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+
+//Funcion que aplica las animaciones a las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills"); 
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if (distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades [0].classList.add("arduino");
+        habilidades [1].classList.add("unity");
+        habilidades [2].classList.add("java");
+        habilidades [3].classList.add("cyc");
+        habilidades [4].classList.add("htmlycss");
+        habilidades [5].classList.add("sonyvegas");
+        habilidades [6].classList.add("photoshop");
+        habilidades [7].classList.add("comunicacion");
+        habilidades [8].classList.add("trabajo");
+        habilidades [9].classList.add("creatividad");
+        habilidades [10].classList.add("dedicacion");
+        habilidades [11].classList.add("pmanagment");
+    }
+}
+
+//Detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+}
